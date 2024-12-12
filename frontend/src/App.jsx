66032e7aa25ca3ba44d9bd8278e91cb83e3d741f
Aman Kumar <aman100170@gmail.com>
2024-12-12 15:11:57 +0530

    Added react query for api fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+import { PingComponent } from './components/atoms/PingComponent'
 import './App.css'
 
 function App() {
+
+  return (
+    <>
+      Hello
+    </>
+  )
   
 }
 
-export default App
+export default App;
